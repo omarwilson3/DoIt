34c932d10eecd330756a486cb1036ee177af284c
Omar Wilson <omar.wilson3@comcast.net>
2016-10-19 12:18:07 -0700

    2 screens now, basic list and another view controller with a back button

---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Omar Wilson on 10/19/16.
+//  Copyright © 2016 Omar Wilson. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
